f591a7a1f2846c29e635feb93922cb4127c81d51
vannguyennd <32758320+vannguyennd@users.noreply.github.com>
2023-06-11 17:36:31 +1000

    Create units_scope.py

---
+++ units_scope.py
@@ -0,0 +1,126 @@
+def start_end_targets_2(p_target_combine):
+    flag_start = False
+    c_start = []
+    c_end = []
+    for i in range(len(p_target_combine)):
+        if flag_start is False:
+            if p_target_combine[i] == '2':
+                c_start.append(i)
+                c_end.append(i)
+            elif p_target_combine[i] == '0':
+                c_start.append(i)
+                flag_start = True
+        else:
+            if p_target_combine[i] == '2':
+                c_end.append(i)
+                flag_start = False
+
+    if len(c_start) == len(c_end) + 1:
+        return c_start[:-1], c_end
+    else:
+        return c_start, c_end
+
+
+def compare(target, predict):
+    for i in range(len(target)):
+        if target[i] != predict[i]:
+            return False
+    return True
+
+
+def compare_target_predict_scope(start_predicts, end_predicts, start_targets, end_targets, predict_combine,
+                                 target_combine):
+
+    target_set = set(zip(start_targets, end_targets))
+    predict_set = set(zip(start_predicts, end_predicts))
+
+    c_true_check = target_set.union(predict_set)
+    c_false = len(target_set - predict_set)
+
+    c_true = 0
+    for i_pair in c_true_check:
+        target = target_combine[i_pair[0]:i_pair[1] + 1]
+        predict = predict_combine[i_pair[0]:i_pair[1] + 1]
+        if compare(target, predict):
+            c_true += 1
+
+    return c_true, c_false
+
+
+def start_end_predicts_2(p_predict_combine):
+    flag_start = False
+    c_start = []
+    c_end = []
+    for i in range(len(p_predict_combine)):
+        if flag_start is False:
+            if p_predict_combine[i] == '2':
+                c_start.append(i)
+                c_end.append(i)
+            elif p_predict_combine[i] == '0':
+                c_start.append(i)
+                flag_start = True
+        else:
+            if p_predict_combine[i] == '2':
+                c_end.append(i)
+                flag_start = False
+
+    if len(c_start) == len(c_end) + 1:
+        return c_start[:-1], c_end
+    else:
+        return c_start, c_end
+
+
+def result_target_predict_scope_recall_2(dir_path, step):
+
+    dir_path = dir_path + 'compare_target_predict/vi_compare_target_predict_valid_2_' + str(step) + '.txt'
+
+    with open(dir_path, 'r') as file:
+        recs = file.readlines()
+
+    target_combine = ''
+    predict_combine = ''
+
+    for i, rec in enumerate(recs):
+        if i % 2 == 0:
+            target_combine += recs[i].split('t: ')[1].split('\n')[0]
+        else:
+            predict_combine += recs[i].split('p: ')[1].split('\n')[0]
+
+    start_targets, end_targets = start_end_targets_2(target_combine)
+    start_predicts, end_predicts = start_end_predicts_2(predict_combine)
+
+    count_true, count_false = compare_target_predict_scope(start_predicts, end_predicts, start_targets, end_targets,
+                                                           predict_combine, target_combine)
+
+    if count_true + count_false == 0:
+        return 0
+    else:
+        return count_true / (count_true + count_false)
+
+
+def result_target_predict_scope_precision_2(dir_path, step):
+
+    dir_path = dir_path + 'compare_target_predict/vi_compare_target_predict_valid_2_' + str(step) + '.txt'
+
+    with open(dir_path, 'r') as file:
+        recs = file.readlines()
+
+    target_combine = ''
+    predict_combine = ''
+
+    for i, rec in enumerate(recs):
+        if i % 2 == 0:
+            target_combine += recs[i].split('t: ')[1].split('\n')[0]
+        else:
+            predict_combine += recs[i].split('p: ')[1].split('\n')[0]
+
+    start_targets, end_targets = start_end_targets_2(target_combine)
+    start_predicts, end_predicts = start_end_predicts_2(predict_combine)
+
+    count_true, count_false = compare_target_predict_scope(start_targets, end_targets, start_predicts, end_predicts,
+                                                           predict_combine, target_combine)
+
+    if count_true + count_false == 0:
+        return 0
+    else:
+        return count_true / (count_true + count_false)
